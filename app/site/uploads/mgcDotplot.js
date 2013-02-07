dc96e6133580e544465dd83a6e01dcd324bb6559
gmp26 <gmp26@cam.ac.uk>
2013-02-07 11:54:12 +0000

    code clean up

--- app/site/uploads/mgcDotplot.js
+++ app/site/uploads/mgcDotplot.js
@@ -1,7 +1,8 @@
 app.directive('mgcDotplot', function factory(funcGen, settings) {
   // define constants and helpers used for the directive
   // ...
-
+  'use strict';
+  
 	var dump = function(name, obj) {
 		angular.forEach(obj, function(value, key) {
 			console.log(name+"."+key+"="+value);
@@ -58,53 +59,28 @@ app.directive('mgcDotplot', function factory(funcGen, settings) {
 		element.css({left:pot[0]+"px",top:pot[1]+"px"});
 	};
 
-  var dirdef = {
+  return {
     restrict: 'E', // the directive can be invoked only by using  tag in the template
     scope: false,
-		compile: function(tElement, tAttrs, transclude) {
-			//dump("tElement", tElement);
-			//dump("tAttrs", tAttrs);
-			//dump("transclude", transclude);
-			//tElement.html(fg.define(tAttrs.f).f);
-			
-			
-			return function (scope, element, attrs) {
-		    // initialization, done once per my-directive tag in template. If my-directive is within an
-		    // ng-repeat-ed template then it will be called every time ngRepeat creates a new copy of the template.
+	link: function (scope, element, attrs) {
 
-				var f = fg.define(scope.f);
+		var f = fg.define(scope.f);
 
-		    //dump("scope", scope);
-				//dump("element", element);
-				//dump("attrs", attrs);
-				//console.log(fg.define(scope.f).f);
+		scope.$watch('f', function(newVal, oldVal) {
+			//console.log("newVal="+f);
+			try {
+				f = fg.define(scope.f);
+				redraw(scope.dot, element, f);
+			}
+			catch(e) {}
+		});
 
-				//var f = fg.define(attrs.f);
-
-				//console.log(element.html());
-				//element.html("foo");
-
-				scope.$watch('f', function(newVal, oldVal) {
-					//console.log("newVal="+f);
-					try {
-						f = fg.define(scope.f);
-						redraw(scope.dot, element, f);
-					}
-					catch(e) {};
-				});
-
-		    // whenever the bound 'exp' expression changes, execute this 
-		    scope.$watch('x', function (newVal, oldVal) {
-		      // ...
-					var pot = toPlot(scope.dot); 
-					element.addClass("dot");
-					element.addClass(fill(scope.dot, f));
-					element.css({left:pot[0]+"px",top:pot[1]+"px"});
-		    });
-		  };
-			
-		}
+		scope.$watch('x', function (newVal, oldVal) {
+				var pot = toPlot(scope.dot); 
+				element.addClass("dot");
+				element.addClass(fill(scope.dot, f));
+				element.css({left:pot[0]+"px",top:pot[1]+"px"});
+		});
+	}
   };
-
-	return dirdef;
 });
